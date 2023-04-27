d49ac01c9417a6f7df56de19bcc4ec7456447b9e
Uzo-Felix <uzochukwuonyekwelu03@gmail.com>
2023-04-27 15:40:05 +0100

    fixes

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -1,7 +1,7 @@
 import ai from './ai.png';
 import ecommerce from './ecommerce.jpg';
 import logo from './logo.svg';
-import gallery04 from './gallery04';
+import gallery04 from './gallery04.png';
 
 
 export default {ai, ecommerce, logo, gallery04};
